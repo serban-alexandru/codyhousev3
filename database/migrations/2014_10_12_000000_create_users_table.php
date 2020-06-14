15b8fe8fa7fe715a0c6c1d621399397642ee9883
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-06-14 11:28:13 +0800

    Update Users migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique();
+            $table->bigInteger('previous_permission')->nullable();
+            $table->bigInteger('permission');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
