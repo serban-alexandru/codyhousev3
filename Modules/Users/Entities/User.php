3ff018237a337a17301fe10e8128f4f4d450bab9
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-06-14 20:36:43 +0800

    Add User module models

---
+++ Modules/Users/Entities/User.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Modules\Users\Entities;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'username', 'name', 'email', 'password', 'permission',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * Get the user's `permission` value and set it either Active/ Inactive as `status`.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function getStatusAttribute()
+    {
+        return ($this->permission < 1) ? 'Inactive' : 'Active';
+    }
+}
