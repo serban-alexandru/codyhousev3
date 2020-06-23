39eea7b496a083295739aff8ade2c41259786976
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-06-23 15:06:21 +0800

    Add modal for edit user form

--- Modules/Users/Resources/views/partials/modals.blade.php
+++ Modules/Users/Resources/views/partials/modals.blade.php
@@ -48,7 +48,7 @@
 <!-- 👇 Full Screen Modal -->
 <div class="modal modal--animate-translate-down flex flex-center bg-contrast-higher bg-opacity-90% padding-md js-modal custom-modal-hide-body-scroll" id="modal-add-user">
   <div class="modal__content width-100% max-width-xs bg radius-md shadow-md flex flex-column" role="alertdialog" aria-labelledby="modal-add-user-title" aria-describedby="modal-description-4">
-    <form action="{{ url('/admin/users/add') }}" id="modal-form-add-user" method="post">
+    <form action="{{ url('admin/users/add') }}" id="modal-form-add-user" method="post">
       <header class="bg-contrast-lower padding-y-sm padding-x-md flex items-center justify-between flex-shrink-0">
         <h4 class="text-truncate" id="modal-add-user-title">Add User</h4>
 
@@ -91,5 +91,34 @@
     </form>
   </div><!-- /.modal__content -->
 
+</div><!-- /.modal -->
+<!-- Full Screen Modal End -->
+
+<!-- 👇 Full Screen Modal -->
+<div class="modal modal--animate-translate-down flex flex-center bg-contrast-higher bg-opacity-90% padding-md js-modal custom-modal-hide-body-scroll" id="modal-edit-user">
+  <div class="modal__content width-100% max-width-xs bg radius-md shadow-md flex flex-column" role="alertdialog" aria-labelledby="modal-edit-user-title" aria-describedby="modal-description-4">
+    <form action="#" method="POST" id="modal-edit-user-form" class="modal-form"> @csrf
+      <header class="bg-contrast-lower padding-y-sm padding-x-md flex items-center justify-between flex-shrink-0">
+        <h4 class="text-truncate" id="modal-edit-user-title">Edit User</h4>
+
+        <button class="reset modal__close-btn modal__close-btn--inner js-modal__close js-tab-focus">
+          <svg class="icon" viewBox="0 0 20 20">
+            <title>Close modal window</title>
+            <g fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="2">
+              <line x1="3" y1="3" x2="17" y2="17" />
+              <line x1="17" y1="3" x2="3" y2="17" />
+            </g>
+          </svg>
+        </button>
+      </header>
+      <div id="ajax-edit-user-form">Loading...</div><!-- /#ajax-edit-user-form -->
+      <footer class="padding-y-sm padding-x-md bg shadow-md flex-shrink-0">
+        <div class="flex justify-end gap-xs">
+          <button type="button" class="btn btn--subtle js-modal__close">Cancel</button>
+          <button type="submit" class="btn btn--primary">Save</button>
+        </div>
+      </footer>
+    </form>
+  </div><!-- /.modal__content -->
 </div><!-- /.modal -->
 <!-- Full Screen Modal End -->
