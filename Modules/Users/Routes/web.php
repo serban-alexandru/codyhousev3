04eb4f33a0b55f3660b6c999f371403d20ad9d73
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-06-23 17:27:44 +0800

    Clean up routes on Users module

--- Modules/Users/Routes/web.php
+++ Modules/Users/Routes/web.php
@@ -15,37 +15,37 @@ Auth::routes();
 
 Route::get('/login/ajax',[
     'as'   => 'login.ajax',
-    'uses' => '\Modules\Users\Http\Controllers\Auth\LoginController@ajaxShowForm'
+    'uses' => 'Auth\LoginController@ajaxShowForm'
 ]);
 
-Route::get('/logout', '\Modules\Users\Http\Controllers\Auth\LoginController@logout');
+Route::get('/logout', 'Auth\LoginController@logout');
 
 Route::get('/register/ajax',[
   'as'   => 'register.ajax',
-  'uses' => '\Modules\Users\Http\Controllers\Auth\RegisterController@ajaxShowForm'
+  'uses' => 'Auth\RegisterController@ajaxShowForm'
 ]);
 
 Route::get('/resetpassword',[
   'as'   => 'password.reset.ajax',
-  'uses' => '\Modules\Users\Http\Controllers\Auth\ForgotPasswordController@ajaxShowForm'
+  'uses' => 'Auth\ForgotPasswordController@ajaxShowForm'
 ]);
 
-Route::group(['middleware' => 'auth'], function(){
-  Route::group(['middleware' => 'role:admin'], function(){
+Route::middleware('auth', 'role:admin')->group(function(){
 
-    Route::get('admin/users', 'UsersController@index');
-    Route::get('admin/users/add', 'UsersController@create');
-    Route::post('admin/users/store', 'UsersController@store');
-    Route::get('admin/users/edit/{id}', 'UsersController@edit');
-    Route::post('admin/users/update/{id}', 'UsersController@update');
-    Route::get('admin/users/suspend/{id}', 'UsersController@suspend');
-    Route::get('admin/users/activate/{id}', 'UsersController@activate');
-    Route::get('admin/users/trash/{id}', 'UsersController@trash');
-    Route::get('admin/users/delete/{id}', 'UsersController@destroy');
-    Route::get('admin/users/delete/{id}', 'UsersController@destroy');
-    Route::get('admin/users/empty-trash', 'UsersController@emptyTrash');
-    Route::post('admin/users/bulk-suspend', 'UsersController@bulkSuspend');
-    Route::post('admin/users/bulk-delete', 'UsersController@bulkDelete');
-
+  Route::prefix('admin')->group(function(){
+    Route::get('users', 'UsersController@index');
+    Route::get('users/add', 'UsersController@create');
+    Route::post('users/store', 'UsersController@store');
+    Route::get('users/edit/{id}', 'UsersController@edit');
+    Route::post('users/update/{id}', 'UsersController@update');
+    Route::get('users/suspend/{id}', 'UsersController@suspend');
+    Route::get('users/activate/{id}', 'UsersController@activate');
+    Route::get('users/trash/{id}', 'UsersController@trash');
+    Route::get('users/delete/{id}', 'UsersController@destroy');
+    Route::get('users/delete/{id}', 'UsersController@destroy');
+    Route::get('users/empty-trash', 'UsersController@emptyTrash');
+    Route::post('users/bulk-suspend', 'UsersController@bulkSuspend');
+    Route::post('users/bulk-delete', 'UsersController@bulkDelete');
   });
+
 });
