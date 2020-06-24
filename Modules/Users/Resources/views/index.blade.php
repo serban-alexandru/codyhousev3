dc71fbe4d24a1b17b69b889f9711b53a9f6e1665
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-06-24 12:32:05 +0800

    Format index view of Users module

--- Modules/Users/Resources/views/index.blade.php
+++ Modules/Users/Resources/views/index.blade.php
@@ -1,23 +1,23 @@
 @extends('admin::layouts.master')
 
+
 @section('content')
 
-@include('users::partials.users-nav')
+  @include('users::partials.users-nav')
 
-@include('users::partials.modals')
+  @include('users::partials.modals')
 
-<section>
-  <div class="container max-width-lg">
-    <div class="grid gap-md@md">
-      @include('users::partials.sidebar')
-
-      <main class="position-relative padding-top-md z-index-1 col-12@md">
-        <div id="site-table-with-pagination-container">
-          @include('users::partials.table')
-        </div><!-- /#site-table-with-pagination-container -->
-      </main>
-    </div><!-- /.grid -->
-  </div><!-- /.container -->
-</section>
+  <section>
+    <div class="container max-width-lg">
+      <div class="grid gap-md@md">
+        @include('users::partials.sidebar')
+        <main class="position-relative padding-top-md z-index-1 col-12@md">
+          <div id="site-table-with-pagination-container">
+            @include('users::partials.table')
+          </div><!-- /#site-table-with-pagination-container -->
+        </main>
+      </div><!-- /.grid -->
+    </div><!-- /.container -->
+  </section>
 
 @endsection
