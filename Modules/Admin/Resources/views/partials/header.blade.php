caeb018ec37833c942e3c29910895178f31b53c4
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-08-22 23:54:28 +0800

    Add user avatar on header navigation

--- Modules/Admin/Resources/views/partials/header.blade.php
+++ Modules/Admin/Resources/views/partials/header.blade.php
@@ -22,13 +22,17 @@
       <div class="dropdown js-dropdown">
         <div class="mega-nav__icon-btn dropdown__wrapper inline-block">
           <a href="#0" class="color-inherit dropdown__trigger">
-            <svg class="icon" viewBox="0 0 24 24">
-              <title>Go to account settings</title>
-              <g class="icon__group" fill="none" stroke="currentColor" stroke-linecap="square" stroke-miterlimit="10" stroke-width="2">
-                <circle cx="12" cy="6" r="4" />
-                <path d="M12 13a8 8 0 00-8 8h16a8 8 0 00-8-8z" />
-              </g>
-            </svg>
+            @if(Auth::user()->getMedia('avatars')->last())
+              <img src="{{ Auth::user()->getMedia('avatars')->last()->getFullUrl('thumb') }}" style="width: 24px; height: 24px; border-radius: 50%; vertical-align: middle;" alt="Logged in user avatar,">
+            @else
+              <svg class="icon" viewBox="0 0 24 24">
+                <title>Go to account settings</title>
+                <g class="icon__group" fill="none" stroke="currentColor" stroke-linecap="square" stroke-miterlimit="10" stroke-width="2">
+                  <circle cx="12" cy="6" r="4" />
+                  <path d="M12 13a8 8 0 00-8 8h16a8 8 0 00-8-8z" />
+                </g>
+              </svg>
+            @endif
           </a>
 
           <ul class="dropdown__menu" aria-label="submenu">
@@ -148,13 +152,17 @@
             <div class="dropdown js-dropdown">
               <div class="mega-nav__icon-btn dropdown__wrapper inline-block">
                 <a href="#0" class="color-inherit flex height-100% width-100% flex-center dropdown__trigger">
-                  <svg class="icon" viewBox="0 0 24 24">
-                    <title>Go to account settings</title>
-                    <g class="icon__group" fill="none" stroke="currentColor" stroke-linecap="square" stroke-miterlimit="10" stroke-width="2">
-                      <circle cx="12" cy="6" r="4" />
-                      <path d="M12 13a8 8 0 00-8 8h16a8 8 0 00-8-8z" />
-                    </g>
-                  </svg>
+                  @if(Auth::user()->getMedia('avatars')->last())
+                    <img src="{{ Auth::user()->getMedia('avatars')->last()->getFullUrl('thumb') }}" style="width: 24px; height: 24px; border-radius: 50%;" alt="Logged in user avatar,">
+                  @else
+                    <svg class="icon" viewBox="0 0 24 24">
+                      <title>Go to account settings</title>
+                      <g class="icon__group" fill="none" stroke="currentColor" stroke-linecap="square" stroke-miterlimit="10" stroke-width="2">
+                        <circle cx="12" cy="6" r="4" />
+                        <path d="M12 13a8 8 0 00-8 8h16a8 8 0 00-8-8z" />
+                      </g>
+                    </svg>
+                  @endif
                 </a>
 
                 <ul class="dropdown__menu" aria-label="submenu">
