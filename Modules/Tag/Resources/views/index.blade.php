ae97eb638461fa05cf98822c823e628cf2441f09
Youngstar <typham2@gmail.com>
2020-09-05 20:32:59 +0700

    Copy files from Post to Tag

--- Modules/Tag/Resources/views/index.blade.php
+++ Modules/Tag/Resources/views/index.blade.php
@@ -1,9 +1,24 @@
-@extends('tag::layouts.master')
-
+@extends('admin::layouts.master')
 @section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('tag.name') !!}
-    </p>
+  @include('post::partials.modals')
+  <section>
+    <div class="container max-width-lg margin-top-xs">
+      <div class="grid gap-md@md">
+        @include('post::partials.sidebar')
+        <main class="position-relative padding-top-md z-index-1 col-12@md">
+          @include('post::partials.control')
+          <div class="bg radius-md padding-md shadow-sm">
+            <div id="site-table-with-pagination-container">
+              @include('post::partials.table')
+            </div><!-- /#site-table-with-pagination-container -->
+          </div><!-- /.bg radius-md padding-md shadow-sm -->
+        </main>
+      </div><!-- /.grid -->
+    </div><!-- /.container -->
+  </section>
 @endsection
+
+@push('module-scripts')
+<!-- MODULE'S CUSTOM SCRIPT -->
+  @include('post::partials.script-js')
+@endpush
---
+++ Modules/Tag/Resources/views/partials/script-js.blade.php
@@ -0,0 +1,59 @@
+@auth
+
+<script src="https://cdn.jsdelivr.net/npm/@editorjs/editorjs@latest"></script>
+<script src="https://cdn.jsdelivr.net/npm/@editorjs/header@latest"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/@editorjs/link@latest"></script> -->
+<script src="https://cdn.jsdelivr.net/npm/@editorjs/raw@latest"></script>
+<script src="https://cdn.jsdelivr.net/npm/@editorjs/simple-image@latest"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/@editorjs/image@latest"></script> -->
+<script src="https://cdn.jsdelivr.net/npm/@editorjs/checklist@latest"></script>
+<script src="https://cdn.jsdelivr.net/npm/@editorjs/embed@latest"></script>
+<script src="https://cdn.jsdelivr.net/npm/@editorjs/quote@latest"></script>
+<script src="https://cdn.jsdelivr.net/npm/@editorjs/list@latest"></script>
+
+<script>
+  (function(){
+    const editor = new EditorJS({
+      /**
+      * Id of Element that should contain Editor instance
+      */
+      holder: 'editorjs',
+      tools: {
+        header: Header,
+        raw: RawTool,
+        image: SimpleImage,
+        embed: Embed,
+        quote: Quote,
+        checklist: {
+          class: Checklist,
+          inlineToolbar: true,
+        },
+        list: {
+          class: List,
+          inlineToolbar: true,
+        }
+      }
+    });
+  })();
+</script>
+
+<script>
+  (function(){
+
+    // load content when user clicked on sidebar links
+    $(document).on('click', '.site-load-content a', function (e) {
+      e.preventDefault();
+      var $this = $(this);
+      var url = $this.attr('href');
+
+      $('meta[name="current-url"]').attr('content', url);
+      console.log(url);
+
+      // loads page content inside this element
+      $('#site-table-with-pagination-container').load(url);
+    });
+
+    console.log('loaded ???');
+  })();
+</script>
+@endauth
