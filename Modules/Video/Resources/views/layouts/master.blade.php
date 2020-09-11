d211f9570bbeccab7d3ad5e2f5e98ef35ea96c9d
Youngstar <typham2@gmail.com>
2020-09-11 12:03:25 +0700

    Copy files from Post to Video

--- Modules/Video/Resources/views/layouts/master.blade.php
+++ Modules/Video/Resources/views/layouts/master.blade.php
@@ -4,16 +4,16 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Module Video</title>
+        <title>Module Post</title>
 
        {{-- Laravel Mix - CSS File --}}
-       {{-- <link rel="stylesheet" href="{{ mix('css/video.css') }}"> --}}
+       {{-- <link rel="stylesheet" href="{{ mix('css/post.css') }}"> --}}
 
     </head>
     <body>
         @yield('content')
 
         {{-- Laravel Mix - JS File --}}
-        {{-- <script src="{{ mix('js/video.js') }}"></script> --}}
+        {{-- <script src="{{ mix('js/post.js') }}"></script> --}}
     </body>
 </html>
