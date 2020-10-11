576d00c60b6b124e4e4ebf787ba2f7d6d232757a
Jonjie Viduya <jonjieviduya@gmail.com>
2020-10-12 02:12:25 +0800

    Added new controller for home page, and showing post. This controller is also responsible for every public pages created.

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DB;
+use Illuminate\Http\Request;
+use Modules\Post\Entities\Post;
+
+class PagesController extends Controller
+{
+
+	// protected $post_per_load = 20;
+	protected $post_per_load = 5;
+
+	public function home()
+	{
+		$posts = DB::table('posts')
+			->select(['id', 'title', 'seo_page_title', 'thumbnail', 'thumbnail_medium'])
+			->where('is_published', 1)
+			->where('is_deleted', 0)
+			->paginate($this->post_per_load);
+
+		return view('site1.index', compact('posts'));
+	}
+
+	public function showPost($slug, $id)
+	{
+		$post = Post::findOrFail($id);
+
+		return view('site1.pages.post', compact('post'));
+	}
+
+}
