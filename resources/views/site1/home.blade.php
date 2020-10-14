243bdbb7e568d3984b37cddd3622b2690f735c4f
Youngstar <typham2@gmail.com>
2020-10-14 08:56:42 +0700

    Clean up #3

--- resources/views/site1/home.blade.php
+++ resources/views/site1/home.blade.php
@@ -1,5 +1,4 @@
 @extends('site1.layouts.app')
 @section('content')
-@include('site1.partials.draggable-gallery')
-@include('site1.partials.article-gallery')
+@include('partials.posts.justified-grid')
 @endsection
