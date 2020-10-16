3d24aeaff9f32c2c30299737e9af6fe0ad22bbf4
Jonjie Viduya <jonjieviduya@gmail.com>
2020-10-16 17:26:01 +0800

    • Fix $post variable undefined error. • Remove unnecessary comments and imported Route class.

--- routes/web.php
+++ routes/web.php
@@ -1,56 +1,39 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
-
-Route::get('/', 'PagesController@home');
-
 // Show post
 Route::get('post/{slug}/{id}', [
   'as' => 'post.show',
   'uses' => 'PagesController@showPost'
 ]);
 
-Route::get('/site1',function(){
-    return view('site1.index');
-  });
+Route::get('/', 'PagesController@home');
 
-  Route::get('/site1/blog',function(){
-    return view('site1.pages.blog');
-  });
+Route::get('/site1', 'PagesController@home');
 
-  Route::get('/site1/profile',function(){
-    return view('site1.pages.profile');
-  });
+Route::get('/site1/blog',function(){
+  return view('site1.pages.blog');
+});
 
-  Route::get('/site1/post',function(){
-    return view('site1.pages.post');
-  });
+Route::get('/site1/profile',function(){
+  return view('site1.pages.profile');
+});
 
-  Route::get('/site1/tag',function(){
-    return view('site1.pages.tag');
-  });
+Route::get('/site1/post',function(){
+  return view('site1.pages.post');
+});
 
-  Route::get('/site1/user-controlpanel',function(){
-    return view('site1.pages.user-controlpanel');
-  });
+Route::get('/site1/tag',function(){
+  return view('site1.pages.tag');
+});
 
-  Route::get('/site1/home',function(){
-    return view('site1.home');
-  });
+Route::get('/site1/user-controlpanel',function(){
+  return view('site1.pages.user-controlpanel');
+});
 
-  Route::get('/site2',function(){
-    return view('site2.index');
-  });
+Route::get('/site1/home',function(){
+  return view('site1.home');
+});
+
+Route::get('/site2',function(){
+  return view('site2.index');
+});
