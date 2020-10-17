2b0cd03a7d5af6081f2d976e3c41b54e06646e58
Youngstar <typham2@gmail.com>
2020-10-17 14:28:02 +0700

    Update featured card classes

--- resources/views/partials/posts/featured-post.blade.php
+++ resources/views/partials/posts/featured-post.blade.php
@@ -13,11 +13,10 @@
       </div>
   
       <div class="col-6@md">
-        <div class="stories">
-            <ol class="stories__list">
+        <div class="featured">
 
-              <li class="stories__story">
-                <a href="#0" class="stories__img-wrapper">
+              <li class="featured__story">
+                <a href="#0" class="featured__img-wrapper">
                   <figure>
                     <img src="{{ asset('assets/img/team-img-1.jpg') }}" alt="Image description">
                   </figure>
@@ -31,41 +30,17 @@
                     </a>
                   
                     <div class="author__content">
-                      <h4 class="stories__metadata">by <a href="#0" rel="author">Olivia Gribben</a></h4>
+                      <h4 class="featured__metadata">by <a href="#0" rel="author">Olivia Gribben</a></h4>
                     </div>
                   </div>
-                  <p class="stories__metadata padding-bottom-xs"><time datetime="2020-05-10">April 8, 2020</time> <span class="stories__separator" role="separator"></span> <a href="#0">Podcast</a></p>
+                  <p class="featured__metadata padding-bottom-xs"><time datetime="2020-05-10">April 8, 2020</time> <span class="featured__separator" role="separator"></span> <a href="#0">Podcast</a></p>
                 </div>
               </li>
 
-              
-          
-                            <li class="stories__story">
-                <a href="#0" class="stories__img-wrapper">
+              <li class="featured__story">
+                <a href="#0" class="featured__img-wrapper">
                   <figure>
-                    <img src="{{ asset('assets/img/team-img-2.jpg') }}" alt="Image description">
-                  </figure>
-                </a>
-          
-                <div class="featured__headline line-height-xl v-space-sm text-sm">
-                  <h4 class="padding-bottom-md"><a href="#0">It was going to be a lonely trip. It was going to be a lonely trip</a></h4>
-                  <div class="author author--minimal padding-bottom-xs">
-                    <a href="#0" class="author__img-wrapper">
-                      <img src="{{ asset('assets/img/team-img-2.jpg') }}" alt="Author picture">
-                    </a>
-                  
-                    <div class="author__content">
-                      <h4 class="stories__metadata">by <a href="#0" rel="author">Olivia Gribben</a></h4>
-                    </div>
-                  </div>
-                  <p class="stories__metadata padding-bottom-xs"><time datetime="2020-05-10">April 8, 2020</time> <span class="stories__separator" role="separator"></span> <a href="#0">Podcast</a></p>
-                </div>
-              </li>
-          
-              <li class="stories__story">
-                <a href="#0" class="stories__img-wrapper">
-                  <figure>
-                    <img src="{{ asset('assets/img/team-img-3.jpg') }}" alt="Image description">
+                    <img src="{{ asset('assets/img/team-img-1.jpg') }}" alt="Image description">
                   </figure>
                 </a>
           
@@ -77,17 +52,17 @@
                     </a>
                   
                     <div class="author__content">
-                      <h4 class="stories__metadata">by <a href="#0" rel="author">Olivia Gribben</a></h4>
+                      <h4 class="featured__metadata">by <a href="#0" rel="author">Olivia Gribben</a></h4>
                     </div>
                   </div>
-                  <p class="stories__metadata padding-bottom-xs"><time datetime="2020-05-10">April 8, 2020</time> <span class="stories__separator" role="separator"></span> <a href="#0">Podcast</a></p>
+                  <p class="featured__metadata padding-bottom-xs"><time datetime="2020-05-10">April 8, 2020</time> <span class="featured__separator" role="separator"></span> <a href="#0">Podcast</a></p>
                 </div>
               </li>
 
-              <li class="stories__story">
-                <a href="#0" class="stories__img-wrapper">
+              <li class="featured__story">
+                <a href="#0" class="featured__img-wrapper">
                   <figure>
-                    <img src="{{ asset('assets/img/team-img-3.jpg') }}" alt="Image description">
+                    <img src="{{ asset('assets/img/team-img-1.jpg') }}" alt="Image description">
                   </figure>
                 </a>
           
@@ -99,10 +74,32 @@
                     </a>
                   
                     <div class="author__content">
-                      <h4 class="stories__metadata">by <a href="#0" rel="author">Olivia Gribben</a></h4>
+                      <h4 class="featured__metadata">by <a href="#0" rel="author">Olivia Gribben</a></h4>
                     </div>
                   </div>
-                  <p class="stories__metadata padding-bottom-xs"><time datetime="2020-05-10">April 8, 2020</time> <span class="stories__separator" role="separator"></span> <a href="#0">Podcast</a></p>
+                  <p class="featured__metadata padding-bottom-xs"><time datetime="2020-05-10">April 8, 2020</time> <span class="featured__separator" role="separator"></span> <a href="#0">Podcast</a></p>
+                </div>
+              </li>
+
+              <li class="featured__story">
+                <a href="#0" class="featured__img-wrapper">
+                  <figure>
+                    <img src="{{ asset('assets/img/team-img-1.jpg') }}" alt="Image description">
+                  </figure>
+                </a>
+          
+                <div class="featured__headline line-height-xl v-space-sm text-sm">
+                  <h4 class="padding-bottom-md"><a href="#0">It was going to be a lonely trip. It was going to be a lonely trip</a></h4>
+                  <div class="author author--minimal padding-bottom-xs">
+                    <a href="#0" class="author__img-wrapper">
+                      <img src="{{ asset('assets/img/team-img-1.jpg') }}" alt="Author picture">
+                    </a>
+                  
+                    <div class="author__content">
+                      <h4 class="featured__metadata">by <a href="#0" rel="author">Olivia Gribben</a></h4>
+                    </div>
+                  </div>
+                  <p class="featured__metadata padding-bottom-xs"><time datetime="2020-05-10">April 8, 2020</time> <span class="featured__separator" role="separator"></span> <a href="#0">Podcast</a></p>
                 </div>
               </li>
           
