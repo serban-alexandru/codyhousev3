d539afd23800ebe35d637ac59c25b7cc0d7447f8
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-10-19 15:54:23 +0800

    Update Tag database seeder

--- Modules/Tag/Database/Seeders/TagDatabaseSeeder.php
+++ Modules/Tag/Database/Seeders/TagDatabaseSeeder.php
@@ -16,6 +16,6 @@ class TagDatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call("OthersTableSeeder");
+        $this->call(TagCategoriesTableSeeder::class);
     }
 }
