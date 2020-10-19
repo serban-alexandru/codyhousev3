4e7f41cecd3ef08723c45c2a7783480e1a352a60
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-10-19 19:40:08 +0800

    Set tag and tag category to have 1 to 1 relationship

--- Modules/Tag/Entities/Tag.php
+++ Modules/Tag/Entities/Tag.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     protected $guarded = ['id'];
+
+    /**
+     * Get the tag category record associated with the tag.
+     *
+     * @return void
+     */
+    public function tag()
+    {
+        return $this->hasOne('Modules\Tag\Entities\TagCategory');
+    }
 }
