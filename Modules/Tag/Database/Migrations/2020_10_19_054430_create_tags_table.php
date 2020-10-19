21160f0301817ba1d0da45b7e6c494bd14da7e7a
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-10-19 15:11:21 +0800

    Create tags and tag categories migrations

---
+++ Modules/Tag/Database/Migrations/2020_10_19_054430_create_tags_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('tag_category_id')->constrained('tag_categories');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('seo_title')->nullable();
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
