4100073862bf5a9048c33ad0c03bb736e3776e0a
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-12-08 13:47:31 +0800

    Update draggable gallery simple itag

--- app/View/Components/DraggableGallerySimpleItag.php
+++ app/View/Components/DraggableGallerySimpleItag.php
@@ -22,31 +22,7 @@ class DraggableGallerySimpleItag extends Component
         $posts = [];
 
         if ($tags) {
-            $tags_collection = Tag::whereIn('name', $tags)->get();
-
-            // Get middle table `posts_tags`
-            $posts_tags = PostsTag::all();
-
-
-            // Get only items from `posts_tags` that is on `tags`
-            $filtered_posts_tags = $posts_tags->filter(function($post_tag, $key) use ($tags_collection){
-                return $tags_collection->contains($post_tag->tag_id);
-            });
-
-
-            // Convert `posts_tags` collection to `posts`
-            $posts = $filtered_posts_tags->map(function($post_tag, $key){
-                    return $post_tag->post; // via `belongsTo` method
-            });
-
-            $posts = $posts->unique()->sortByDesc('created_at')->where('is_published', true);
-
-            if ($limit) {
-                $posts = $posts->slice(0, $limit);
-            }
-
-            $posts = $posts->all();
-
+            $posts = Post::getByTagNames($tags, $limit);
         }else{
             // Else if not set, then just get latest posts
             $posts = Post::where(
