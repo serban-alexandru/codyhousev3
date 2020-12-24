b1a2f85d13fbf6a49d80c895c0b56743d136d6d9
Youngstar <typham2@gmail.com>
2020-12-24 11:30:59 +0700

    Back to top to site1 index

--- resources/views/site1/index.blade.php
+++ resources/views/site1/index.blade.php
@@ -7,6 +7,10 @@
 	<!-- NEW via blade component -->
 	<x-featured-post />
 
+	<a class="back-to-top js-back-to-top" href="#" data-offset="100" data-duration="300">
+		<svg class="icon" viewBox="0 0 16 16"><title>Go to top of page</title><g stroke-width="1" stroke="currentColor"><polyline fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="15.5,11.5 8,4 0.5,11.5 "></polyline></g></svg>
+	  </a>
+
 	<div class="margin-bottom-xl"></div>
 
 	<div class="container max-width-lg">
