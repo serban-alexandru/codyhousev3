8acf797ff96d498f36580fc42b97637f006770d3
Youngstar <typham2@gmail.com>
2020-12-28 16:28:37 +0700

    Delete unused files and add 1 col table

--- Modules/Admin/Resources/views/index.blade.php
+++ Modules/Admin/Resources/views/index.blade.php
@@ -1,18 +1,20 @@
 @extends('admin::layouts.master')
 @section('content')
-<section>
-  <div class="container max-width-lg margin-top-md">
-      <main class="flex items-center grid gap-sm z-index-1 col-15@md">
-        @include('admin::widgets.users')
-        @include('admin::widgets.visitors')
-        @include('admin::widgets.posts')
-        @include('admin::widgets.visitors')
-        @include('admin::widgets.posts')
-      </main>
-  </div><!-- /.container -->
-</section>
+  <section>
+    <div class="container max-width-lg margin-top-xs">
+      <div class="grid gap-md@md">
+        @include('video::partials.sidebar')
+        <main class="position-relative padding-top-md z-index-1 col-12@md">
+          <div class="bg radius-md padding-md shadow-sm">
+              @include('admin::partials.table')
+          </div><!-- /.bg radius-md padding-md shadow-sm -->
+        </main>
+      </div><!-- /.grid -->
+    </div><!-- /.container -->
+  </section>
 @endsection
+
 @push('module-scripts')
 <!-- MODULE'S CUSTOM SCRIPT -->
-  @include('users::partials.script-js')
+  @include('video::partials.script-js')
 @endpush
---
+++ Modules/Admin/Resources/views/partials/table.blade.php
@@ -0,0 +1,109 @@
+<table class="cl-table cl-table--expanded@xs width-100% js-cl-table" aria-label="Column Oriented Table Example">
+  <thead class="cl-table__header cl-table__header--sticky">
+    <tr class="cl-table__row">
+      <th class="cl-table__cell cl-table__cell--th" aria-hidden="true"></th>
+      <th class="cl-table__cell cl-table__cell--th text-right">Spring</th>
+      <th class="cl-table__cell cl-table__cell--th text-right">Summer</th>
+      <th class="cl-table__cell cl-table__cell--th text-right">Autumn</th>
+      <th class="cl-table__cell cl-table__cell--th text-right">Winter</th>
+    </tr>
+  </thead>
+  
+  <tbody class="cl-table__body">
+    <tr class="cl-table__row">
+      <th class="cl-table__cell cl-table__cell--th text-left">High Temperature °C</th>
+      <td class="cl-table__cell text-right">26°C</td>
+      <td class="cl-table__cell text-right">30°C</td>
+      <td class="cl-table__cell text-right">25°C</td>
+      <td class="cl-table__cell text-right">20°C</td>
+    </tr>
+
+    <tr class="cl-table__row">
+      <th class="cl-table__cell cl-table__cell--th text-left">High Temperature °F</th>
+      <td class="cl-table__cell text-right">78.8°F</td>
+      <td class="cl-table__cell text-right">86°F</td>
+      <td class="cl-table__cell text-right">77°F</td>
+      <td class="cl-table__cell text-right">68°F</td>
+    </tr>
+
+    <tr class="cl-table__row">
+      <th class="cl-table__cell cl-table__cell--th text-left">Low Temperature °C</th>
+      <td class="cl-table__cell text-right">18°C</td>
+      <td class="cl-table__cell text-right">20°C</td>
+      <td class="cl-table__cell text-right">15°C</td>
+      <td class="cl-table__cell text-right">8°C</td>
+    </tr>
+
+    <tr class="cl-table__row">
+      <th class="cl-table__cell cl-table__cell--th text-left">Low Temperature °F</th>
+      <td class="cl-table__cell text-right">64.4°C</td>
+      <td class="cl-table__cell text-right">68°C</td>
+      <td class="cl-table__cell text-right">59°C</td>
+      <td class="cl-table__cell text-right">46.4°C</td>
+    </tr>
+
+    <tr class="cl-table__row">
+      <th class="cl-table__cell cl-table__cell--th text-left">Average Temperature °C</th>
+      <td class="cl-table__cell text-right">22°C</td>
+      <td class="cl-table__cell text-right">26°C</td>
+      <td class="cl-table__cell text-right">20°C</td>
+      <td class="cl-table__cell text-right">14°C</td>
+    </tr>
+
+    <tr class="cl-table__row">
+      <th class="cl-table__cell cl-table__cell--th text-left">Average Temperature °F</th>
+      <td class="cl-table__cell text-right">71.6°C</td>
+      <td class="cl-table__cell text-right">78.8°C</td>
+      <td class="cl-table__cell text-right">68°C</td>
+      <td class="cl-table__cell text-right">57.2°C</td>
+    </tr>
+
+    <tr class="cl-table__row">
+      <th class="cl-table__cell cl-table__cell--th text-left">Average Rainfall</th>
+      <td class="cl-table__cell text-right">40mm</td>
+      <td class="cl-table__cell text-right">30mm</td>
+      <td class="cl-table__cell text-right">55mm</td>
+      <td class="cl-table__cell text-right">50mm</td>
+    </tr>
+
+    <tr class="cl-table__row">
+      <th class="cl-table__cell cl-table__cell--th text-left">Average Rain Days</th>
+      <td class="cl-table__cell text-right">10</td>
+      <td class="cl-table__cell text-right">5</td>
+      <td class="cl-table__cell text-right">15</td>
+      <td class="cl-table__cell text-right">14</td>
+    </tr>
+
+    <tr class="cl-table__row">
+      <th class="cl-table__cell cl-table__cell--th text-left">Probability of Precipitation</th>
+      <td class="cl-table__cell text-right">50%</td>
+      <td class="cl-table__cell text-right">40%</td>
+      <td class="cl-table__cell text-right">55%</td>
+      <td class="cl-table__cell text-right">55%</td>
+    </tr>	
+
+    <tr class="cl-table__row">
+      <th class="cl-table__cell cl-table__cell--th text-left">Humidity</th>
+      <td class="cl-table__cell text-right">70%</td>
+      <td class="cl-table__cell text-right">65%</td>
+      <td class="cl-table__cell text-right">80%</td>
+      <td class="cl-table__cell text-right">85%</td>
+    </tr>
+
+    <tr class="cl-table__row">
+      <th class="cl-table__cell cl-table__cell--th text-left">Wind</th>
+      <td class="cl-table__cell text-right">10 km/h</td>
+      <td class="cl-table__cell text-right">8 km/h</td>
+      <td class="cl-table__cell text-right">20 km/h</td>
+      <td class="cl-table__cell text-right">24 km/h</td>
+    </tr>
+
+    <tr class="cl-table__row">
+      <th class="cl-table__cell cl-table__cell--th text-left">Visibility</th>
+      <td class="cl-table__cell text-right">30 km</td>
+      <td class="cl-table__cell text-right">30 km</td>
+      <td class="cl-table__cell text-right">25 km</td>
+      <td class="cl-table__cell text-right">20 km</td>
+    </tr>
+  </tbody>
+</table>
