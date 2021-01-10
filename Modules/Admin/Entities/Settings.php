150c54bfbce890d6be13d34b95fe16022b6e4911
stevanstajo0 <77220346+stevanstajo0@users.noreply.github.com>
2021-01-10 08:29:21 +0100

    Fix migration issue

--- Modules/Admin/Entities/Settings.php
+++ Modules/Admin/Entities/Settings.php
@@ -3,18 +3,21 @@
 namespace Modules\Admin\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Schema;
 
 class Settings extends Model
 {
     public static function getSiteSettings()
     {
-        $settings = Settings::all();
-
         $setting_data = array();
-        foreach($settings as $data) {
-          $setting_data[$data['key']] = $data['value'];
-        }
+        if (Schema::hasTable('settings')) {
+          $settings = Settings::all();
 
+          foreach($settings as $data) {
+            $setting_data[$data['key']] = $data['value'];
+          }
+        }
+        
         return $setting_data;
     }
 }
