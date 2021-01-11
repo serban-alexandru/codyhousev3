27cc0685d1b3fbe30587be2c52e23fad13f9c2e9
stevanstajo0 <77220346+stevanstajo0@users.noreply.github.com>
2021-01-11 17:26:20 +0100

    Add new layout for homepage only

--- resources/views/site1/layouts/home.blade.php
+++ resources/views/site1/layouts/home.blade.php
@@ -4,12 +4,14 @@
     <!-- favicon 👇 -->
     <link rel="icon" type="image/svg+xml" href="{{ !empty($settings_data['favicon']) ? asset($settings_data['favicon']) : asset('assets/img/favicon.svg') }}">
 
-    <title>@yield('title-tag')</title>
+    <title>{{ !empty($settings_data['page_title']) ? $settings_data['page_title'] : '' }}</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <meta name="title" content="{{ !empty($settings_data['meta_title']) ? $settings_data['meta_title'] : '' }}">
+    
     <script>document.getElementsByTagName("html")[0].className += " js";</script>
     @yield('in-head')
     <script>
@@ -48,4 +50,3 @@
     @yield('before-end')
   </body>
   </html>
-
