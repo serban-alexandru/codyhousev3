549eb33636c5cfad634ad15ba3a8c560ba2b3f06
stevanstajo0 <77220346+stevanstajo0@users.noreply.github.com>
2021-01-21 04:16:22 +0100

    Remove placeholder icon from user dashboard page

--- Modules/Users/Resources/views/dashboard/sub-nav.blade.php
+++ Modules/Users/Resources/views/dashboard/sub-nav.blade.php
@@ -2,10 +2,6 @@
   <div class="container max-width-lg flex items-center justify-between">
     <div class="">
       <button class="btn btn--primary margin-right-md" aria-controls="modal-add-article">Add</button>
-      <a class="margin-right-md" href="#0"><svg class="icon header-v2__nav-dropdown-ico" viewbox="0 0 32 32">
-        <title>single-01</title>
-        <g fill="#a8a8a8"><path d="M27.711,21.887C25.332,21.026,21.368,20,16,20s-9.332,1.026-11.711,1.887 C2.322,22.598,1,24.486,1,26.587V31c0,0.552,0.448,1,1,1h28c0.552,0,1-0.448,1-1v-4.413C31,24.486,29.678,22.598,27.711,21.887z"></path>
-        <path fill="#a8a8a8" d="M16,18c4.411,0,8-3.589,8-8V8c0-4.411-3.589-8-8-8S8,3.589,8,8v2C8,14.411,11.589,18,16,18z"></path></g></svg></a>
             
       <form class="expandable-search js-expandable-search" action="{{ url('dashboard') }}" method="GET">
         <label class="sr-only" for="expandable-search">Search</label>
