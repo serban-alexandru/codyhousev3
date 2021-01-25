42161c32e460c9af7e3d27fc31ce1512d6d0cc4d
stevanstajo0 <77220346+stevanstajo0@users.noreply.github.com>
2021-01-25 04:24:37 +0100

    Create Components - User Profile & Single Post

---
+++ app/View/Components/Posts/SinglePost.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\View\Components\Posts;
+
+use Illuminate\View\Component;
+
+use Modules\Post\Entities\Post;
+
+class SinglePost extends Component
+{
+    public $post;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($id)
+    {
+        $post = Post::where(
+            [
+                'id' => $id,
+                'is_published' => true,
+                'is_pending'   => false,
+                'is_deleted'   => false
+            ]
+        )->first();
+
+        $this->post = $post;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\View\View|string
+     */
+    public function render()
+    {
+        return view('components.posts.single-post');
+    }
+}
