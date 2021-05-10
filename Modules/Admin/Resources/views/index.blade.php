5ebf03a71e1f56d1178c067c03df2ac4dea73c2b
Youngstar <typham2@gmail.com>
2021-05-10 12:31:26 +0700

    Change post widget

--- Modules/Admin/Resources/views/index.blade.php
+++ Modules/Admin/Resources/views/index.blade.php
@@ -1,5 +1,7 @@
 @extends('admin::layouts.master')
 @section('content')
+<!-- Tag modal Start -->
+
   <section>
     <div class="container max-width-adaptive-sm margin-top-lg">
       <div class="grid gap-md@md">
@@ -10,10 +12,19 @@
               <div class="flex flex-wrap gap-xs items-center">
           
                 <div class="line-height-xs">
-
-                  <h3 class="text-left">Post content</h3>
-                  <button class="btn btn--subtle">Add Image</button>
-                  <button class="btn btn--subtle">Add Tags</button>
+                  
+                  <h3 class="text-left">Create Post</h3>
+                  <div class="file-upload inline-block">
+                    <label for="upload2" class="file-upload__label btn btn--primary">
+                      <span class="flex items-center">
+                        <svg class="icon" viewBox="0 0 24 24" aria-hidden="true"><g fill="none" stroke="currentColor" stroke-width="2"><path  stroke-linecap="square" stroke-linejoin="miter" d="M2 16v6h20v-6"></path><path stroke-linejoin="miter" stroke-linecap="butt" d="M12 17V2"></path><path stroke-linecap="square" stroke-linejoin="miter" d="M18 8l-6-6-6 6"></path></g></svg>
+                        
+                        <span class="margin-left-xxs file-upload__text file-upload__text--has-max-width">Add Image</span>
+                      </span>
+                    </label> 
+                  
+                    <input type="file" class="file-upload__input" name="upload2" id="upload2" multiple>
+                  </div>
                 </div>
               </div>
             </div>
@@ -41,10 +52,12 @@
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><title>file</title><g fill="#000000"><path d="M14,0H3A1,1,0,0,0,2,1V23a1,1,0,0,0,1,1H21a1,1,0,0,0,1-1V8H15a1,1,0,0,1-1-1Z" fill="#000000"></path><polygon points="21.414 6 16 6 16 0.586 21.414 6"></polygon></g></svg>
                 <span class="menu-bar__label">Create Page</span>
               </li>    
+
+              <button class="btn btn--primary margin-right-xs">Save</button>
+              <button class="btn btn--primary">Publish</button>
             
             </menu>
           </a>
-
 
       </div><!-- /.grid -->
     </div><!-- /.container -->
