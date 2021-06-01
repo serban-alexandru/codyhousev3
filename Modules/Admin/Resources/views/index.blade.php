c9044f56e9dc34af4c189099cb8520b19a3226bf
stevan <77220346+stevan@users.noreply.github.com>
2021-06-01 16:47:13 +0800

    Fix some style issue on Postbox module

--- Modules/Admin/Resources/views/index.blade.php
+++ Modules/Admin/Resources/views/index.blade.php
@@ -29,14 +29,14 @@
                   <circle cx="14.5" cy="7.5" r="1.5" /></svg>
               </li>
 
-              <li class="menu-bar__item btn-load-box {{ $type == 'post' ? 'menu-control--active' : '' }}" role="menuitem" box-type="post">
+              <li class="menu-bar__item btn-load-box {{ $type == 'post' ? 'menu-bar-control--active' : '' }}" role="menuitem" box-type="post">
                 <svg class="icon menu-bar__icon" aria-hidden="true" viewBox="0 0 12 12">
                   <path d="M10.121.293a1,1,0,0,0-1.414,0L1,8,0,12l4-1,7.707-7.707a1,1,0,0,0,0-1.414Z"></path>
                 </svg>
                 <span class="menu-bar__label">Create Post</span>
               </li>
 
-              <li class="menu-bar__item btn-load-box {{ $type == 'page' ? 'menu-control--active' : '' }}" role="menuitem" box-type='page'>
+              <li class="menu-bar__item btn-load-box {{ $type == 'page' ? 'menu-bar-control--active' : '' }}" role="menuitem" box-type='page'>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><title>file</title><g fill="#000000"><path d="M14,0H3A1,1,0,0,0,2,1V23a1,1,0,0,0,1,1H21a1,1,0,0,0,1-1V8H15a1,1,0,0,1-1-1Z" fill="#000000"></path><polygon points="21.414 6 16 6 16 0.586 21.414 6"></polygon></g></svg>
                 <span class="menu-bar__label">Create Page</span>
               </li>
