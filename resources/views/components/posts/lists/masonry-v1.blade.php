7d3c9052139177d1761cce4357880fa63c444065
Youngstar <typham2@gmail.com>
2021-06-02 21:20:35 +0700

    Change masonry username text size

--- resources/views/components/posts/lists/masonry-v1.blade.php
+++ resources/views/components/posts/lists/masonry-v1.blade.php
@@ -55,7 +55,7 @@
             </a>
           </figure>
           <div class="user-cell__content text-component line-height-xs v-space-xxs text-sm line-height-md">
-            <p><a class="color-contrast-low" href="{{ route('pages.profile.user', $post->username) }}">{{ $post->username }}</a></p>
+            <p><a class="color-contrast-low text-sm" href="{{ route('pages.profile.user', $post->username) }}">{{ $post->username }}</a></p>
             <p class="color-contrast-medium text-xs">{{ $post->created_at->format('F j, Y') }}</p>
           </div>
         </div>
