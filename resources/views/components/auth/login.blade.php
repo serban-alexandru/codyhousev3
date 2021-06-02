b9b85d16026484c6e459a2e9d1c952092660d895
Youngstar <typham2@gmail.com>
2021-06-02 16:05:34 +0700

    Add text link to login and register blade

--- resources/views/components/auth/login.blade.php
+++ resources/views/components/auth/login.blade.php
@@ -2,6 +2,7 @@
 @section('content')
 <section class="container max-width-adaptive-xs margin-top-md margin-bottom-md text-center">
     <h1>Login</h1>
+    <p class="margin-top-sm">Don't have an account? <a href="{{ url('/site2/register') }}">Get started</a></p>
     <x-auth.login-form />
 </section>
 @endsection
--- resources/views/components/auth/register.blade.php
+++ resources/views/components/auth/register.blade.php
@@ -2,6 +2,7 @@
 @section('content')
 <section class="container max-width-adaptive-xs margin-top-md margin-bottom-md text-center">
     <h1>Register</h1>
+    <p class="margin-top-sm">Have an account? <a href="{{ url('/site2/login') }}">Login here</a></p>
     <x-auth.register-form />
 </section>
 @endsection
