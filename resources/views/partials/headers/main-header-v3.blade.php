d4f6551a7abc723d30e86d4ed1cfd2c4126bb94e
Youngstar <typham2@gmail.com>
2021-06-08 14:48:55 +0700

    Add bg gradient scss component

--- resources/views/partials/headers/main-header-v3.blade.php
+++ resources/views/partials/headers/main-header-v3.blade.php
@@ -1,6 +1,6 @@
 <header class="dr-nav-header margin-bottom-xl">
     <div class="container position-relative height-100% flex items-center max-width-adaptive-lg">
-      <a class="dr-nav-header__logo" href="index.html">
+      <a class="dr-nav-header__logo" href="/">
         <svg viewBox="0 0 64 64">
           <title>Go to homepage</title>
           <path fill="currentColor" d="M32 1a31 31 0 1031 31A31 31 0 0032 1zm15.189 17.262a5.693 5.693 0 00-2.169 1.686 11.279 11.279 0 00-1.891 3.552l-9.281 25.119q-.495-.043-1.547-.043-1.011 0-1.483.043L19.582 21.076a5.357 5.357 0 00-1.3-2.116 2.47 2.47 0 00-1.471-.7v-.881q2.642.129 6.7.129 4.49 0 6.681-.129v.881a6.764 6.764 0 00-2.374.4 1.236 1.236 0 00-.718 1.24 6.34 6.34 0 00.537 2.062l7.756 19.7 5.416-14.592a17.5 17.5 0 001.224-5.37 3.216 3.216 0 00-.934-2.567 4.374 4.374 0 00-2.761-.87v-.881q2.964.129 5.5.129 2.019 0 3.351-.129z" />
@@ -41,37 +41,24 @@
             <li>
               <a class="dr-nav__link" href="#0">
                 <span>News</span>
-                <span>35</span>
+                <span><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12"><title>right-arrow</title><g fill="#707070"><path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" fill="#707070"></path></g></svg></span>
               </a>
             </li>
   
             <li>
               <a class="dr-nav__link" href="#0">
                 <span>Creative Industry</span>
-                <span>17</span>
+                <span><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12"><title>right-arrow</title><g fill="#707070"><path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" fill="#707070"></path></g></svg></span>
               </a>
             </li>
   
             <li>
               <a class="dr-nav__link" href="#0">
                 <span>Video</span>
-                <span>12</span>
+                <span><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12"><title>right-arrow</title><g fill="#707070"><path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" fill="#707070"></path></g></svg></span>
               </a>
             </li>
   
-            <li>
-              <a class="dr-nav__link" href="#0">
-                <span>Photography</span>
-                <span>24</span>
-              </a>
-            </li>
-  
-            <li>
-              <a class="dr-nav__link" href="#0">
-                <span>Tutorials</span>
-                <span>18</span>
-              </a>
-            </li>
           </ul>
         </nav>
   
