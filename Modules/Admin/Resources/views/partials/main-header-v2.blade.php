79fe6820dd2256a75decdfe24d90826b00cb06fd
Youngstar <typham2@gmail.com>
2021-06-09 18:45:22 +0700

    Clean up admin menu

--- Modules/Admin/Resources/views/partials/main-header-v2.blade.php
+++ Modules/Admin/Resources/views/partials/main-header-v2.blade.php
@@ -93,7 +93,7 @@
                     <li class="header-v2__nav-item header-v2__nav-item--label">Main Type</li>
                     <li class="header-v2__nav-item"><a href="{{ url('admin/posts') }}" class="header-v2__nav-link">Posts</a></li>
                     <li class="header-v2__nav-item"><a href="{{ url('admin/pages') }}" class="header-v2__nav-link">Pages</a></li>
-                    <li class="header-v2__nav-item"><a href="#0" class="header-v2__nav-link">Link Three</a></li>
+                    <li class="header-v2__nav-item"><a href="#0" class="header-v2__nav-link">Gifs</a></li>
                   </ul>
 
                   <ul class="header-v2__nav-list">
@@ -111,37 +111,13 @@
                   </ul>
 
                   <ul class="header-v2__nav-list">
-                    <li class="header-v2__nav-item header-v2__nav-item--label">Archives</li>
-                    <li class="header-v2__nav-item"><a href="#0" class="header-v2__nav-link">Tags</a></li>
-                    <li class="header-v2__nav-item"><a href="#0" class="header-v2__nav-link">Link Two</a></li>
-                    <li class="header-v2__nav-item"><a href="#0" class="header-v2__nav-link">Link Three</a></li>
+                    <li class="header-v2__nav-item header-v2__nav-item--label">Others</li>
+                    <li class="header-v2__nav-item"><a href="{{ url('admin/menus') }}" class="header-v2__nav-link">Menus</a></li>
                   </ul>
 
                 </li>
 
-                <li class="header-v2__nav-item header-v2__nav-item--divider" role="separator"></li>
-
-                <li class="header-v2__nav-item">
-                  <ul class="header-v2__nav-list header-v2__nav-list--title-desc">
-                    <li class="header-v2__nav-item">
-                      <a href="#0" class="header-v2__nav-link">
-                        <svg class="header-v2__nav-icon" aria-hidden="true" width="32" height="32" viewBox="0 0 32 32">
-                          <circle fill="var(--color-accent)" opacity="0.2" cx="16" cy="16" r="16" />
-                          <circle cx="11.5" cy="10.5" r="3.5" fill="var(--color-accent)" />
-                          <path d="M22,12,4.729,27.352a15.982,15.982,0,0,0,26.24-5.742Z" fill="var(--color-accent)" />
-                        </svg>
-
-                        <div>
-                          <strong>Sub nav item</strong>
-                          <small>Lorem ipsum dolor sit amet.</small>
-                        </div>
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-          </li>
+               
 
         </ul>
       </nav>
