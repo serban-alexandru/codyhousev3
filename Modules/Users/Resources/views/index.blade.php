e956ebf98f83b0d921b7bce674c7f250cd0a0f01
Youngstar <typham2@gmail.com>
2021-06-10 18:34:04 +0700

    Modify user module index

--- Modules/Users/Resources/views/index.blade.php
+++ Modules/Users/Resources/views/index.blade.php
@@ -3,15 +3,10 @@
   @include('users::partials.modals')
   <section>
     <div class="container max-width-lg margin-top-xs">
-      <div class="grid gap-md@md">
+      <div class="grid">
         @include('users::partials.sidebar')
         <main class="position-relative padding-top-md z-index-1 col-12@md">
-          @include('users::partials.control')
-          <div class="bg radius-md padding-md shadow-sm">
-            <div id="site-table-with-pagination-container">
-              @include('users::partials.table')
-            </div><!-- /#site-table-with-pagination-container -->
-          </div><!-- /.bg radius-md padding-md shadow-sm -->
+          @include('users::partials.index')
         </main>
       </div><!-- /.grid -->
     </div><!-- /.container -->
