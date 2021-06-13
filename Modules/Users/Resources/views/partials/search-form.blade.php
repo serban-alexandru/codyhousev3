2c7c061dffcb260b4961286165ee34d36010a0c5
Youngstar <typham2@gmail.com>
2021-06-13 12:44:56 +0700

    Deleted users module unused files

--- Modules/Users/Resources/views/partials/search-form.blade.php
+++
@@ -1,7 +0,0 @@
-<form action="{{ url('admin/users') }}" method="GET">
-  <input type="hidden" name="limit" value="{{$limit ?? ''}}">
-  <input type="hidden" name="sort" value="{{$sort ?? ''}}">
-  <input type="hidden" name="order" value="{{$order ?? ''}}">
-
-  <input class="form-control width-100%" type="reset search" name="q" value="{{ $q ?? '' }}" id="megasite-search" placeholder="Search..." aria-label="Search">
-</form>
