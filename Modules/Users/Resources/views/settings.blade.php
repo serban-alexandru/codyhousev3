c29761a595c423829a983fd9b4461229b20f6c67
Youngstar <typham2@gmail.com>
2021-06-13 12:27:54 +0700

    Del cover-photo-component. Merged to settings

--- Modules/Users/Resources/views/settings.blade.php
+++ Modules/Users/Resources/views/settings.blade.php
@@ -28,7 +28,21 @@
           @endif
 
           {{-- COVER PHOTO --}}
-          @include('users::partials.cover-photo-component')
+          <div>
+            <div class="alert-cover-photo hidden">
+              <button class="btn-cover-photo" id="btnCancel" type="button" onclick="location.reload()">Cancel</button>
+              <button class="btn-cover-photo" id="btnUploadCoverPhoto" type="button">Update</button>
+            </div>
+            <div>
+              <input type="file" name="upload_image" id="uploadImage" style="display: none;">
+              <div id="uploaded_image"></div>
+            </div>
+          
+            <div id="imageDemo"></div>
+            
+            <input type="hidden" value="" id="base64Image">
+          </div>
+          
 
           <form action="{{ url('users/settings/save') }}" method="post" enctype="multipart/form-data">
             @csrf
